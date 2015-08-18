bb038742d55b6223bbe07a34fe22d9a34db3b484
Sarah Bindman <sbindman@spatialdev.com>
2015-08-18 12:53:52 -0700

    testing group custom fields

--- ckanext/cadasta/plugin.py
+++ ckanext/cadasta/plugin.py
@@ -173,105 +173,105 @@ class CadastaPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, Default
             })
         return schema
 
-    #user schema update
-    def _modify_user_schema(self, schema):
-        schema.update({
-            'phone': [toolkit.get_validator('ignore_missing'),
-                            toolkit.get_converter('convert_to_extras')]
-        })
-        return schema
-
-    def create_user_schema(self):
-        schema = super(CadastaPlugin, self).create_user_schema()
-        schema = self._modify_user_schema(schema)
-        return schema
-
-    def update_user_schema(self):
-        schema = super(CadastaPlugin, self).update_user_schema()
-        schema = self._modify_user_schema(schema)
-        return schema
-
-
-
-
-    def is_fallback(self):
-        # Return True to register this plugin as the default handler for
-        # package types not handled by any other IDatasetForm plugin.
-        return True
-
-    def package_types(self):
-        # This plugin doesn't handle any special package types, it just
-        # registers itself as the default (above).
-        return []
-
-
-    def get_helpers(self):
-        return { }
-
-
-    plugins.implements(plugins.IGroupForm, inherit=True)
-    plugins.implements(plugins.IConfigurer)
-
-    ## IGroupForm
-
-
-    def group_types(self):
-        return ['organization']
+    # #user schema update
+    # def _modify_user_schema(self, schema):
+    #     schema.update({
+    #         'phone': [toolkit.get_validator('ignore_missing'),
+    #                         toolkit.get_converter('convert_to_extras')]
+    #     })
+    #     return schema
     #
-    def form_to_db_schema_options(self, options):
-        ''' This allows us to select different schemas for different
-        purpose eg via the web interface or via the api or creation vs
-        updating. It is optional and if not available form_to_db_schema
-        should be used.
-        If a context is provided, and it contains a schema, it will be
-        returned.
-        '''
-        schema = options.get('context', {}).get('schema', None)
-        if schema:
-            return schema
-
-        if options.get('api'):
-            if options.get('type') == 'create':
-                return self.form_to_db_schema_api_create()
-            else:
-                return self.form_to_db_schema_api_update()
-        else:
-            return self.form_to_db_schema()
-
-
-    def form_to_db_schema(self):
-        schema = super(CadastaPlugin, self).form_to_db_schema()
-        schema = self._modify_group_schema(schema)
-        return schema
-
-    def _modify_group_schema(self, schema):
-
-        # Import core converters and validators
-        _convert_to_extras = plugins.toolkit.get_converter('convert_to_extras')
-        _ignore_missing = plugins.toolkit.get_validator('ignore_missing')
-
-        default_validators = [_ignore_missing, _convert_to_extras, unicode]
-        schema.update({
-            'publisher_source_type': default_validators,
-            'publisher_iati_id': default_validators
-        })
-
-        return schema
-
-    def db_to_form_schema(self):
-
-        # Import core converters and validators
-        _convert_from_extras = plugins.toolkit.get_converter('convert_from_extras')
-        _ignore_missing = plugins.toolkit.get_validator('ignore_missing')
-        _ignore = plugins.toolkit.get_validator('ignore')
-        _not_empty = plugins.toolkit.get_validator('not_empty')
-
-        schema = super(CadastaPlugin, self).form_to_db_schema()
-
-        default_validators = [_convert_from_extras, _ignore_missing]
-        schema.update({
-            'publisher_source_type': default_validators,
-            'publisher_iati_id': default_validators
-        })
-
-        return schema
+    # def create_user_schema(self):
+    #     schema = super(CadastaPlugin, self).create_user_schema()
+    #     schema = self._modify_user_schema(schema)
+    #     return schema
+    #
+    # def update_user_schema(self):
+    #     schema = super(CadastaPlugin, self).update_user_schema()
+    #     schema = self._modify_user_schema(schema)
+    #     return schema
+    #
+    #
+    #
+    #
+    # def is_fallback(self):
+    #     # Return True to register this plugin as the default handler for
+    #     # package types not handled by any other IDatasetForm plugin.
+    #     return True
+    #
+    # def package_types(self):
+    #     # This plugin doesn't handle any special package types, it just
+    #     # registers itself as the default (above).
+    #     return []
+    #
+    #
+    # def get_helpers(self):
+    #     return { }
+    #
+    #
+    # plugins.implements(plugins.IGroupForm, inherit=True)
+    # plugins.implements(plugins.IConfigurer)
+    #
+    # ## IGroupForm
+    #
+    #
+    # def group_types(self):
+    #     return ['organization']
+    # #
+    # def form_to_db_schema_options(self, options):
+    #     ''' This allows us to select different schemas for different
+    #     purpose eg via the web interface or via the api or creation vs
+    #     updating. It is optional and if not available form_to_db_schema
+    #     should be used.
+    #     If a context is provided, and it contains a schema, it will be
+    #     returned.
+    #     '''
+    #     schema = options.get('context', {}).get('schema', None)
+    #     if schema:
+    #         return schema
+    #
+    #     if options.get('api'):
+    #         if options.get('type') == 'create':
+    #             return self.form_to_db_schema_api_create()
+    #         else:
+    #             return self.form_to_db_schema_api_update()
+    #     else:
+    #         return self.form_to_db_schema()
+    #
+    #
+    # def form_to_db_schema(self):
+    #     schema = super(CadastaPlugin, self).form_to_db_schema()
+    #     schema = self._modify_group_schema(schema)
+    #     return schema
+    #
+    # def _modify_group_schema(self, schema):
+    #
+    #     # Import core converters and validators
+    #     _convert_to_extras = plugins.toolkit.get_converter('convert_to_extras')
+    #     _ignore_missing = plugins.toolkit.get_validator('ignore_missing')
+    #
+    #     default_validators = [_ignore_missing, _convert_to_extras, unicode]
+    #     schema.update({
+    #         'publisher_source_type': default_validators,
+    #         'publisher_iati_id': default_validators
+    #     })
+    #
+    #     return schema
+    #
+    # def db_to_form_schema(self):
+    #
+    #     # Import core converters and validators
+    #     _convert_from_extras = plugins.toolkit.get_converter('convert_from_extras')
+    #     _ignore_missing = plugins.toolkit.get_validator('ignore_missing')
+    #     _ignore = plugins.toolkit.get_validator('ignore')
+    #     _not_empty = plugins.toolkit.get_validator('not_empty')
+    #
+    #     schema = super(CadastaPlugin, self).form_to_db_schema()
+    #
+    #     default_validators = [_convert_from_extras, _ignore_missing]
+    #     schema.update({
+    #         'publisher_source_type': default_validators,
+    #         'publisher_iati_id': default_validators
+    #     })
+    #
+    #     return schema
