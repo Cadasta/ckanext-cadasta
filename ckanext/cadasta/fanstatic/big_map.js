f0231f409e405e3ab5a3278e52d57904974850fa
Ryan Whitley <apollolm@gmail.com>
2015-08-28 11:41:11 -0700

    Big map changes.

--- ckanext/cadasta/fanstatic/big_map.js
+++ ckanext/cadasta/fanstatic/big_map.js
@@ -4,47 +4,20 @@ L.mapbox.accessToken = 'pk.eyJ1Ijoic2JpbmRtYW4iLCJhIjoiaENWQnlrVSJ9.0DQyCLWgA0j8
 
 
 
-function displayParcels (map) {
-//    converts string data into a geojson object that can be mapped
 
-    var parcelCollection = $('#data').data();
-    var popupURL = $('#popup_url').data().obj;
-
-    var parcel_string = JSON.stringify(parcelCollection);
-    parcel_string = parcel_string.replace('{"obj":', '');
-    parcel_string = parcel_string.replace('}}]}}', '}}]}');
-
-    var parcel_geoJSON = JSON.parse(parcel_string);
-
-    parcel_layer = L.geoJson(parcel_geoJSON, {
-        style: function (feature) {
-        return {color: 'green'};
-        },
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup("<a href="+ popupURL + layer.feature.name + ">See Parcel Details</a>");
-        }
-    });
-
-    parcel_layer.addTo(parcelsFeatureGroup);
-    map.fitBounds(parcelsFeatureGroup.getBounds());
-
-}
 
 $(document).ready(function() {
 
     var map = L.mapbox.map('big_map', 'mapbox.streets');
 
+    var featureGroup,parcelsFeatureGroup;
+    var parcel_layer, drawControl;
 
     map.on("load", function () {
-        var featureGroup = L.featureGroup().addTo(map);
-        var parcelsFeatureGroup = L.featureGroup().addTo(map);
+        featureGroup = L.featureGroup().addTo(map);
+        parcelsFeatureGroup = L.featureGroup().addTo(map);
 
-        var json = '';
-
-        var parcel_layer;
-
-
-        var drawControl = new L.Control.Draw({
+        drawControl = new L.Control.Draw({
             edit: {
                 featureGroup: featureGroup
             },
@@ -74,22 +47,48 @@ $(document).ready(function() {
     })
 
 
+    function addParcel() {
+        var json = featureGroup.toGeoJSON();
+        var elem = document.getElementById("parcel_geom");
+        elem.value = json.features[0].geometry.coordinates[0];
+    }
+
+
+    parcel_layer.on("click", function (e) {
+        e.layer.options.color = "black";
+        e.layer.options.fill = "black";
+    });
+
+    function displayParcels (map) {
+//    converts string data into a geojson object that can be mapped
+
+    var parcelCollection = $('#data').data();
+    var popupURL = $('#popup_url').data().obj;
+
+    var parcel_string = JSON.stringify(parcelCollection);
+    parcel_string = parcel_string.replace('{"obj":', '');
+    parcel_string = parcel_string.replace('}}]}}', '}}]}');
+
+    var parcel_geoJSON = JSON.parse(parcel_string);
+
+    parcel_layer = L.geoJson(parcel_geoJSON, {
+        style: function (feature) {
+            return {color: 'green'};
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("<a href="+ popupURL + layer.feature.name + ">See Parcel Details</a>");
+        }
+    });
+
+    parcel_layer.addTo(parcelsFeatureGroup);
+    map.fitBounds(parcelsFeatureGroup.getBounds());
+
+}
+
 })
 
 
 
 
-function addParcel() {
-    json = featureGroup.toGeoJSON();
 
-    var elem = document.getElementById("parcel_geom");
-    elem.value = json.features[0].geometry.coordinates[0];
-}
-
-
-
-parcel_layer.on("click", function(e) {
-    e.layer.options.color="black";
-    e.layer.options.fill="black";
-});
 
