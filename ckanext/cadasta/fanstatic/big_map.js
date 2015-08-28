1723dfbbadc559f5408506d4a9917e5cea14bdc6
Ryan Whitley <apollolm@gmail.com>
2015-08-28 11:33:16 -0700

    re-arranging .js

--- ckanext/cadasta/fanstatic/big_map.js
+++ ckanext/cadasta/fanstatic/big_map.js
@@ -1,38 +1,7 @@
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoic2JpbmRtYW4iLCJhIjoiaENWQnlrVSJ9.0DQyCLWgA0j8yBpmvt3bGA';
 
-  var map = L.mapbox.map('big_map', 'mapbox.streets');
 
-  var featureGroup = L.featureGroup().addTo(map);
-  var parcelsFeatureGroup = L.featureGroup().addTo(map);
-
-  var json = '';
-
-  var parcel_layer;
-
-
-  var drawControl = new L.Control.Draw({
-    edit: {
-      featureGroup: featureGroup
-    },
-    draw: {
-        polyline: false,
-        circle: false, // Turns off this drawing tool
-        rectangle: false,
-        marker: false
-    }
-  }).addTo(map);
-
-  map.on('draw:created', function(e) {
-    var layer = e.layer;
-    //make sure there is only one geometry
-    if(featureGroup && featureGroup.getLayers().length!==0){
-      featureGroup.clearLayers();
-    }
-
-    featureGroup.addLayer(layer);
-    addParcel();
-  });
 
 
 function displayParcels (map) {
@@ -61,9 +30,50 @@ function displayParcels (map) {
 
 }
 
-$(document).ready(function(){
-    //move this to the jquery, on document load run this function
-displayParcels (map);
+$(document).ready(function() {
+
+    var map = L.mapbox.map('big_map', 'mapbox.streets');
+
+
+    map.on("load", function () {
+        var featureGroup = L.featureGroup().addTo(map);
+        var parcelsFeatureGroup = L.featureGroup().addTo(map);
+
+        var json = '';
+
+        var parcel_layer;
+
+
+        var drawControl = new L.Control.Draw({
+            edit: {
+                featureGroup: featureGroup
+            },
+            draw: {
+                polyline: false,
+                circle: false, // Turns off this drawing tool
+                rectangle: false,
+                marker: false
+            }
+        }).addTo(map);
+    })
+
+
+    map.on('draw:created', function (e) {
+        var layer = e.layer;
+        //make sure there is only one geometry
+        if (featureGroup && featureGroup.getLayers().length !== 0) {
+            featureGroup.clearLayers();
+        }
+
+        featureGroup.addLayer(layer);
+        addParcel();
+    });
+
+    featureGroup.on("load", function () {
+        displayParcels(map);
+    })
+
+
 })
 
 
