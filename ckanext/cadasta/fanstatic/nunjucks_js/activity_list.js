017e59e24da563f9f5609af7b5a4d904f0aa44da
Sarah Bindman <sbindman@spatialdev.com>
2015-09-03 15:36:08 -0700

    added nunjucks file for activity_list, parcel_list and parcel_details

--- ckanext/cadasta/fanstatic/nunjucks_js/activity_list.js
+++ ckanext/cadasta/fanstatic/nunjucks_js/activity_list.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
     //Hit API and get back response
-    var url = "http://54.69.121.180:3000/show_parcels_list";
+    var url = "http://54.69.121.180:3000/show_activity";
 
     $.ajax(url).done(function (response) {
         //success
             nunjucks.configure('/nunjucks_snippets', {autoescape: true});
-            var res = nunjucks.render('parcel_list.html', response);
+            var res = nunjucks.render('activity_list.html', response);
 
-             $("#project-content").html(res);
+             $("#nunjuck-activity-list").html(res);
     })
     .fail(function () {
         //err
