1c238e826d6940f487c0e57566d61238a8230720
Sarah Bindman <sbindman@spatialdev.com>
2015-09-09 12:31:21 -0700

    added new controller for parcels page

--- ckanext/cadasta/fanstatic/angular_controllers.js
+++ ckanext/cadasta/fanstatic/angular_controllers.js
@@ -11,11 +11,16 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
     //
     // Now set up the states
     $stateProvider
-        .state('state1', {
+        .state('main', {
             url: "/",
             controller:'ProjectOverview',
             templateUrl: "/partials/read.html"
-        });
+        })
+        .state('parcels', {
+            url: "/parcels",
+            controller:'ProjectParcels',
+            templateUrl: "/partials/parcels.html"
+        });;
 });
 
 myApp.controller('ProjectOverview', [ '$scope', '$http', function ($scope, $http) {
@@ -32,6 +37,59 @@ myApp.controller('ProjectOverview', [ '$scope', '$http', function ($scope, $http
 
         response.features.forEach(function(activity) {
             activity.properties.time_created = format_date(activity.properties.time_created);
+        });
+
+        $scope.response = response;
+
+    });
+
+    var format_date = function(date) {
+        var date_object = new Date(date);
+        var month = date_object.getMonth();
+        var day = date_object.getDay();
+        var year = date_object.getFullYear();
+        var date_object_formatted = month + "/" + day + "/" + year;
+
+        return date_object_formatted;
+    }
+
+    var getDescription = function() {
+        var description = $('#ckan-project-description').data().obj;
+        return description;
+    }
+
+    var getTitle = function() {
+        var title = $('#ckan-project-title').data().obj;
+        return title;
+    }
+
+    var getName = function() {
+        var name = $('#ckan-project-name').data().obj;
+        return name;
+    }
+
+    $scope.projectDescription = getDescription();
+    $scope.projectTitle = getTitle();
+    $scope.projectName = getName();
+
+
+}]);
+
+myApp.controller('ProjectParcels', [ '$scope', '$http', function ($scope, $http) {
+
+
+        //Hit API and get back response
+    var url = "http://54.69.121.180:3000/show_parcels_list";
+
+    var req = {
+        method: 'GET',
+        url: url
+    };
+
+    $http(req).success(function (response) {
+
+        response.features.forEach(function(parcel) {
+            parcel.properties.time_created = format_date(parcel.properties.time_created);
         });
 
         $scope.response = response;
